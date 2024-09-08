07fb87ee9521f830037f414f427f0813f8599daa
welsea <welsea.42@gmail.com>
2024-09-08 16:47:19 +0200

    chose word put into map

--- app/ui/game.tsx
+++ app/ui/game.tsx
@@ -1,6 +1,4 @@
 "use client"
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import TextArea from "./textarea";
 import { Grid } from "./gridArea";
 import { useState,useEffect } from "react";
